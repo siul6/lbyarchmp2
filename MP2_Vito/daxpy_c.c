f380b75801955d4e8fd0f0651a0c24b6a823b28a
siul6 <luis_vito@dlsu.edu.ph>
2026-08-07 13:11:08 +0800

    Delete MP2_Vito directory

--- MP2_Vito/daxpy_c.c
+++
@@ -1,9 +0,0 @@
-#include "daxpy.h"
-
-__declspec(noinline) void daxpy_c(int n, double a, const double *x, const double *y, double *z)
-{
-    int i;
-
-    for (i = 0; i < n; i++)
-        z[i] = a * x[i] + y[i];
-}
