97b23e26eeb27e1f81145d1dba205948aa044ce1
siul6 <luis_vito@dlsu.edu.ph>
2026-08-07 13:29:36 +0800

    adjusted function format

--- MP2_Files/MP2_Vito/daxpy_c.c
+++ MP2_Files/MP2_Vito/daxpy_c.c
@@ -1,6 +1,6 @@
 #include "daxpy.h"
 
-__declspec(noinline) void daxpy_c(int n, double a, const double *x, const double *y, double *z)
+void daxpy_c(int n, double a, const double *x, const double *y, double *z)
 {
     int i;
 
